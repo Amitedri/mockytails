ccee91fa18c1c7a7cfd3c4e373cd46ef66ceb7dd
amitedri <amitedri778@gmail.com>
2021-01-19 12:12:10 +0200

    updates

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -8,28 +8,31 @@ const Main = () => {
     const [showResult, setShowResult] = React.useState(false);
     const [resultFromApi, setResultFromApi] = React.useState([]);
 
-    const getDataFromApi = async (keyword) => {
-        const determinLink = (link)=>{
-            switch (link) {
+    const getDataFromApi = async (keyword, linkType) => {
+        const determinLink = () => {
+            switch (linkType) {
                 case 'name':
                     return `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${keyword}`;
+                case 'alcoholic': return `https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=${keyword}`
                     break;
-                
-                default:return link;
+                case 'glass': return `https://www.thecocktaildb.com/api/json/v1/1/filter.php?g=${keyword}`
                     break;
+                case 'ingredients': return `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${keyword}`
+                    break;
+                case 'category': return `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${keyword}`
+                default: return linkType;
             }
         }
-        console.log(determinLink('name'))
+        const link = determinLink(linkType)
+        console.log(link)
         const res = await axios({
             method: 'get',
-            url: `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${keyword}`
+            url: link
         });
-        console.log(res.data.drinks)
-        res.data.drinks ? setShowResult(true) : setShowResult(false);
-        res.data.drinks ? setResultFromApi(res.data.drinks) : setResultFromApi([]);
-
+        res.data?.drinks ? setShowResult(true) : setShowResult(false);
+        res.data?.drinks ? setResultFromApi(res.data.drinks) : setResultFromApi([]);
     }
-    const [showSet, setShowSet] = React.useState(false);
+    const [showSearchBar, setShowSearchBar] = React.useState(false);
 
     const { MainWrapper, Header, H1, H2, Image, ActionButton, SearchButton, ResultWindow, ResLine, SetInputs, SearchInput, InputWrapper, InputDescription, Span, FieldName, FieldsWrapper, FieldValue } = components;
     const mapResults = (list) => {
@@ -43,22 +46,22 @@ const Main = () => {
     const setInputs = () => {
         return (
             <SetInputs>
-                <InputWrapper ><InputDescription><Span>Name</Span></InputDescription><SearchInput placeholder='Type Name...' onInput={(event) => getDataFromApi(event.target.value)} /></InputWrapper>
-                <InputWrapper><InputDescription><Span>Ingredients</Span></InputDescription><SearchInput placeholder='Ingreients...' /></InputWrapper>
-                <InputWrapper><InputDescription><Span>Alcoholic</Span></InputDescription><SearchInput placeholder='Is Alcohlic? ' /></InputWrapper>
-                <InputWrapper><InputDescription><Span>Favourite Alcohol</Span></InputDescription><SearchInput placeholder='favourite...' /></InputWrapper>
-                <InputWrapper><InputDescription><Span>Glass</Span></InputDescription><SearchInput placeholder='Glass...' /></InputWrapper>
+                <InputWrapper><InputDescription><Span>Name</Span></InputDescription><SearchInput placeholder='Type Name...' onInput={(event) => getDataFromApi(event.target.value, 'name')} /></InputWrapper>
+                <InputWrapper><InputDescription><Span>Ingredients</Span></InputDescription><SearchInput placeholder='Ingreients...' onInput={(event) => getDataFromApi(event.target.value, 'ingredients')} /></InputWrapper>
+                <InputWrapper><InputDescription><Span>Alcoholic</Span></InputDescription><SearchInput placeholder='Is Alcohlic? ' onInput={(event) => getDataFromApi(event.target.value, 'alcoholic')} /></InputWrapper>
+                <InputWrapper><InputDescription><Span>Category</Span></InputDescription><SearchInput placeholder='Category...' onInput={(event) => getDataFromApi(event.target.value, 'category')} /></InputWrapper>
+                <InputWrapper><InputDescription><Span>Glass</Span></InputDescription><SearchInput placeholder='Glass...' onInput={(event) => getDataFromApi(event.target.value, 'glass')} /></InputWrapper>
             </SetInputs>
         )
     }
     return <MainWrapper>
-        <SearchButton onClick={() => setShowSet((prevState) => !prevState)}>Search Alcohol</SearchButton>
-        {showSet ? setInputs() : null}
+        <SearchButton onClick={() => setShowSearchBar((prevState) => !prevState)}>Search Alcohol</SearchButton>
+        {showSearchBar ? setInputs() : null}
         <Image src={backy} />
         <Header>
             <H1 src={h1} />
             <H2>SHAKE YOUR COCKTAILS AT ANY PLACE</H2>
-            <ActionButton>EXPLORE</ActionButton>
+            <ActionButton onClick={() => setShowSearchBar((prevState) => !prevState)}>EXPLORE</ActionButton>
         </Header>
         {showResult ? <ResultWindow>
             <FieldsWrapper>
